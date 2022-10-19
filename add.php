20cb6819e09db063e4aa3b82e40dec6dfbc7b51c
jepchub <53791166+jepchub@users.noreply.github.com>
2022-10-19 09:41:58 -0400

    Partial html

--- add.php
+++ add.php
@@ -37,91 +37,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 <!-- Segun definio en el campo input atributo name asi llegara aqui en el post las variables -->
-<!DOCTYPE html>
-<html lang="en">
+<?php require "partials/header.php" ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="container pt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Add New Contact</div>
+        <div class="card-body">
+          <!-- //ANCHOR  - if error -->
+          <?php if ($error) : ?>
+            <p class="text-danger">
+              <?= $error ?>
+            </p>
+          <?php endif ?>
+          <!-- //ANCHOR -  Formulario action es el que nos ayuda con esta peticion-->
+          <form method="POST" action="add.php">
+            <div class="mb-3 row">
+              <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
-  <!-- bootstrap -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.2/darkly/bootstrap.min.css" integrity="sha512-8RiGzgobZQmqqqJYja5KJzl9RHkThtwqP1wkqvcbbbHNeMXJjTaBOR+6OeuoxHhuDN5h/VlgVEjD7mJu6KNQXA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-  <!-- css static content-->
-  <link rel="stylesheet" href="./static/css/index.css">
-  <title>Contacts App</title>
-</head>
-
-<body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand font-weight-bold" href="#">
-        <img class="mr-2" src="./static/img/logo.png" />
-        ContactsApp
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="./index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./add.php">Add Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <main>
-    <div class="container pt-5">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-header">Add New Contact</div>
-            <div class="card-body">
-              <!-- //ANCHOR  - if error -->
-              <?php if ($error) : ?>
-                <p class="text-danger">
-                  <?= $error ?>
-                </p>
-              <?php endif ?>
-              <!-- //ANCHOR -  Formulario action es el que nos ayuda con esta peticion-->
-              <form method="POST" action="add.php">
-                <div class="mb-3 row">
-                  <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-
-                  <div class="col-md-6">
-                    <!-- la variable name define el nombre de variables de como van a llegar en el post -->
-                    <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
-                  </div>
-                </div>
-
-                <div class="mb-3 row">
-                  <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-
-                  <div class="col-md-6">
-                    <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
-                  </div>
-                </div>
-
-                <div class="mb-3 row">
-                  <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </div>
-              </form>
+              <div class="col-md-6">
+                <!-- la variable name define el nombre de variables de como van a llegar en el post -->
+                <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
+              </div>
             </div>
-          </div>
+
+            <div class="mb-3 row">
+              <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
+
+              <div class="col-md-6">
+                <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-  </main>
-</body>
+  </div>
+</div>
 
-</html>
+<?php require "partials/footer.php" ?>
---
+++ partials/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.2/darkly/bootstrap.min.css" integrity="sha512-8RiGzgobZQmqqqJYja5KJzl9RHkThtwqP1wkqvcbbbHNeMXJjTaBOR+6OeuoxHhuDN5h/VlgVEjD7mJu6KNQXA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+  <!-- css static content-->
+  <link rel="stylesheet" href="./static/css/index.css">
+  <title>Contacts App</title>
+</head>
+
+<body>
+  <?php require "navbar.php" ?>
+
+  <main>
+
+    <!-- content here -->
